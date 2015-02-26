49733ec64acc77f67fe9a339b737adf68195beea
Philippe Guillebert <philippe.guillebert@linkfluence.com>
2015-02-26 21:45:23 +0100

    Add a NRPE adapter

--- src/cauchy/jobs/utils.clj
+++ src/cauchy/jobs/utils.clj
@@ -1,4 +1,6 @@
-(ns cauchy.jobs.utils)
+(ns cauchy.jobs.utils
+  (:require [clojure.java.shell :as sh]
+            [clojure.string :as str]))
 
 (defn threshold
   [{:keys [warn crit comp] :as conf} metric]
@@ -6,3 +8,26 @@
    (comp metric crit) "critical"
    (comp metric warn) "warn"
    :else "ok"))
+
+(def rc->state
+  {0 :ok
+   1 :warn
+   2 :critical
+   3 :unknown})
+
+(defn exec-nrpe
+  [cmd]
+  (let [cmdparts (str/split cmd #"\ ")
+        {:keys [exit out err]} (apply sh/sh cmdparts)
+        status-line (last (str/split out #"\n"))
+        [status perfdata] (map str/trim
+                               (str/split status-line #"\|"))
+        state (rc->state exit :unknown)]
+
+    (mapv #(let [[metric value]
+                 (str/split % #"\=")]
+             {:state state
+              :status-line status
+              :service metric
+              :value value})
+          (str/split perfdata #"\ "))))
