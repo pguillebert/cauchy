d2a04a92ef9297cccedd11429c61ca6cfda715f4
Philippe Guillebert <philippe.guillebert@linkfluence.com>
2015-02-26 21:43:30 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -11,11 +11,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.3.0"]
-                 [org.slf4j/log4j-over-slf4j "1.7.6"]
-                 [riemann-clojure-client "0.2.11"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.slf4j/log4j-over-slf4j "1.7.10"]
+                 [riemann-clojure-client "0.3.2"]
                  [jarohen/chime "0.1.6"]
-                 [puppetlabs/trapperkeeper "1.0.0"]
+                 [puppetlabs/trapperkeeper "1.1.0"]
                  [indigenous "0.1.0-SNAPSHOT"]
                  [bultitude "0.2.6"]
                  [sigmund "0.1.1" :exclusions [log4j sigar/sigar-native-deps]]
