80fab17d629f13bb94e366cbca74adc7d260292c
Philippe Guillebert <philippe.guillebert@linkfluence.com>
2015-03-02 15:59:44 +0100

    Prepare release

--- project.clj
+++ project.clj
@@ -10,13 +10,15 @@
                   [:timezone "+1"]]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :signing {:gpg-key "93FEB8D7"}
+  :deploy-repositories [["clojars" {:creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.slf4j/log4j-over-slf4j "1.7.10"]
                  [riemann-clojure-client "0.3.2"]
                  [jarohen/chime "0.1.6"]
                  [puppetlabs/trapperkeeper "1.1.0"]
-                 [indigenous "0.1.0-SNAPSHOT"]
+                 [indigenous "0.1.0"]
                  [bultitude "0.2.6"]
                  [sigmund "0.1.1" :exclusions [log4j sigar/sigar-native-deps]]
                  [stask/sigar-native-deps "1.6.4"]]
