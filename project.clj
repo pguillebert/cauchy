237e94cd59ba5b99be7e1b3d924054220a3c8767
Philippe Guillebert <philippe.guillebert@linkfluence.com>
2015-05-29 15:23:19 +0200

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject cauchy "0.1.0-SNAPSHOT"
-  :description "Cauchy is a client for Riemann"
+(defproject cauchy "0.1.0"
+  :description "Cauchy is an agent for Riemann"
   :url "https://github.com/pguillebert/cauchy"
   :scm {:name "git"
         :url "https://github.com/pguillebert/cauchy"}
@@ -14,10 +14,10 @@
   :deploy-repositories [["clojars" {:creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.slf4j/log4j-over-slf4j "1.7.10"]
+                 [org.slf4j/log4j-over-slf4j "1.7.12"]
                  [riemann-clojure-client "0.3.2"]
                  [jarohen/chime "0.1.6"]
-                 [puppetlabs/trapperkeeper "1.1.0"]
+                 [puppetlabs/trapperkeeper "1.1.1"]
                  [indigenous "0.1.0"]
                  [bultitude "0.2.6"]
                  [sigmund "0.1.1" :exclusions [log4j sigar/sigar-native-deps]]
